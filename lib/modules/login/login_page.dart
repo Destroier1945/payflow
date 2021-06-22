f07655af18229d8efc7f1b6b122fb284297831cf
Destroier1945 <kluge233@hotmail.com>
2021-06-21 22:39:54 -0300

    Aula 2 completada

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgtes/social_login/social_login.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -37,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             Positioned(
-              bottom: size.height * 0.18,
+              bottom: size.height * 0.12,
               left: 0,
               right: 0,
               child: Column(
@@ -52,6 +53,15 @@ class _LoginPageState extends State<LoginPage> {
                       "Organize seus boletos em um só lugar",
                       textAlign: TextAlign.center,
                       style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print('clicou');
+                      },
                     ),
                   ),
                 ],
