45361aa73bd86d671d64b7008383fc106edbcb17
Destroier1945 <kluge233@hotmail.com>
2021-06-21 22:38:35 -0300

    Aula 2 completada

---
+++ winuwp/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\kluge\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\kluge\\NLW\\payflow" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\kluge\\flutter"
+  "PROJECT_DIR=C:\\Users\\kluge\\NLW\\payflow"
+  "FLUTTER_ROOT=C:\\Users\\kluge\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\kluge\\NLW\\payflow\\winuwp\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\kluge\\NLW\\payflow"
+  "FLUTTER_TARGET=C:\\Users\\kluge\\NLW\\payflow\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\kluge\\NLW\\payflow\\.dart_tool\\package_config.json"
+)
